bd4c7dec74c0b1810175ca2cd95e9286aeb4fb71
mldxo <miloszmaculewicz@gmail.com>
2024-03-18 19:20:58 +0100

    Add feast

---
+++ dining_philosophers/src/feast.py
@@ -0,0 +1,12 @@
+"""Feast module for dining philosophers problem"""
+
+class Feast():
+    """Feast class for dining philosophers problem"""
+
+    def __init__(self) -> None:
+        pass
+
+    def run(self) -> None:
+        """Run method for the Feast class"""
+        print("Feast is running")
+        return
