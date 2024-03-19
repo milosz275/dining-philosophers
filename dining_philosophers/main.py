cd4f9e4762e32cc18320cdb6ed67a770b2b2f866
mldxo <miloszmaculewicz@gmail.com>
2024-03-19 11:12:56 +0100

    Add app version

--- dining_philosophers/main.py
+++ dining_philosophers/main.py
@@ -1,10 +1,12 @@
 """Main module for dining_philosophers package"""
 
 from src.feast import Feast
+from version import __version__ as version
 
 def main():
     """Executes main function"""
     app = Feast()
+    app.set_version(version)
     app.run()
     return
 
