c9fa227f75e3d834ace4a4155bf186427691d68e
mldxo <miloszmaculewicz@gmail.com>
2024-03-19 11:13:05 +0100

    Implement app version

--- dining_philosophers/src/feast.py
+++ dining_philosophers/src/feast.py
@@ -4,9 +4,19 @@ class Feast():
     """Feast class for dining philosophers problem"""
 
     def __init__(self) -> None:
-        pass
+        self.__version : str = None
+        return
+    
+    def set_version(self, version: str) -> None:
+        """Set the version of the Feast"""
+        self.__version = version
+        return
+    
+    def version(self) -> str:
+        """Get the version of the Feast"""
+        return self.__version
 
     def run(self) -> None:
         """Run method for the Feast class"""
-        print("Feast is running")
+        print(f"Feast {self.version()} is running")
         return
