465f1d371be527a5347807578aafdd4d1ecd8857
mldxo <miloszmaculewicz@gmail.com>
2024-03-26 18:07:12 +0100

    Update imports

--- dining_philosophers/__init__.py
+++ dining_philosophers/__init__.py
@@ -1,5 +1,4 @@
 """Init file for dining_philosophers package"""
 
-
 from .main import main
-from dining_philosophers.src.feast import Feast
+from dining_philosophers.src.dining_simulation import DiningSimulation
